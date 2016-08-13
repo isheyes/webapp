beff9c2fa95406b6ad21564fe57c42b6c4a2d1c4
zxy <zxy@163.com>
2016-08-14 01:45:08 +0800

    3、通用图文组件类H5ComponentBase参数设置

---
+++ js/H5ComponentBase.js
@@ -0,0 +1,23 @@
+// 基本图文组件对象
+
+var H5ComponentBase = function(name,cfg){
+	var cfg = cfg || {}; //这里运用的短路原则
+	var id = ('h5_c_' + Math.random()).replace('.','_');
+	//把当前的类型添加到样式中进行标记
+	var cls = 'h5_component_'+cfg.type+' h5_component_' + name;
+	var component = $('<div class="h5_component '+cls+'" id="'+id+'">');
+
+	cfg.text  	&& component.text(cfg.text);
+	cfg.width 	&& component.width(cfg.width/2);
+	cfg.height 	&& component.height(cfg.height/2);
+	cfg.css 	&& component.css(cfg.css);
+	cfg.bg 		&& component.css('backgroundImage','url('+cfg.bg+')');
+
+	if (cfg.center === true) {
+		component.css({
+			marginLeft: (cfg.width/4 * -1) + 'px',
+			left:'50%'
+		})
+	}
+	return component;
+}
