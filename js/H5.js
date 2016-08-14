7a2170b8899c52c00a662711cfc5674b99bf438a
zxy <zxy@163.com>
2016-08-14 17:46:35 +0800

    6、整合fullPage.js支持页面切换

--- js/H5.js
+++ js/H5.js
@@ -37,7 +37,15 @@ var H5 = function(){
 	}
 
 	this.loader = function(){
-		this.el.fullpage();
+		this.el.fullpage({
+			onLeave:function(index,nextIndex,direction){
+				$(this).find('.h5_component').trigger('onLeave');
+			},
+			afterLoad:function(anchorLink,index){
+				$(this).find('.h5_component').trigger('onLoad');
+			}
+		});
+		this.page[0].find('.h5_component').trigger('onLoad');
 		this.el.show();
 	}
 	return this;
--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -4,8 +4,8 @@ var H5ComponentBase = function(name,cfg){
 	var cfg = cfg || {}; //这里运用的短路原则
 	var id = ('h5_c_' + Math.random()).replace('.','_');
 	//把当前的类型添加到样式中进行标记
-	var cls = 'h5_component_name_' + name + ' h5_component_'+cfg.type;
-	var component = $('<div class="h5_component '+cls+'" id="'+id+'">');
+	var cls =  'h5_component_'+cfg.type;
+	var component = $('<div class="h5_component '+cls+' h5_component_name_' + name+'" id="'+id+'">');
 
 	cfg.text  	&& component.text(cfg.text);
 	cfg.width 	&& component.width(cfg.width/2);
